5f02eddfb89ce3032d7000b64426f4fc9c6a223c
Emiliano Salazar <salazaremiliano@84gmail.com>
2024-05-28 21:42:06 -0300

    se incorpora testing en invoice

---
+++ src/invoice/interface/invoice.entity.ts
@@ -0,0 +1,6 @@
+export interface Iinvoice {
+    idInvoice: number,
+    invoiceDate: Date,
+    total_without_iva: number,
+    total_with_iva: number
+}
--- src/invoice/invoice.controller.spec.ts
+++ src/invoice/invoice.controller.spec.ts
@@ -1,9 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InvoiceController } from './invoice.controller';
 import { InvoiceService } from './invoice.service';
+import { Invoice } from './entities/invoice.entity';
+import { Iinvoice } from './interface/invoice.entity';
 
 describe('InvoiceController', () => {
-  let controller: InvoiceController;
+  let invoiceController: InvoiceController;
+  let invoiceService: InvoiceService;
+
+  const mockedArrayInvoice: Iinvoice[] = [
+    {
+      "invoiceDate": new Date("2024-05-21T23:20:50.000Z"),
+      "total_without_iva": 21216,
+      "total_with_iva": 25671,
+      "idInvoice": 1
+    },
+    {
+      "invoiceDate": new Date("2024-05-21T23:27:51.000Z"),
+      "total_without_iva": 21216,
+      "total_with_iva": 25671,
+      "idInvoice": 2
+    },
+    {
+      "invoiceDate": new Date("2024-05-21T23:28:09.000Z"),
+      "total_without_iva": 21216,
+      "total_with_iva": 25671,
+      "idInvoice": 3
+    }
+  ]
+
+  const mockInvoiceRepository = {
+    createInvoice: jest.fn((newInvoice: Invoice) => {
+      mockedArrayInvoice.push(newInvoice)
+      return newInvoice;
+    }),
+    findAllInvoice: jest.fn(() => mockedArrayInvoice),
+    findOneInvoice: jest.fn((idInvoive: number))
+      findInvoiceByUser:
+      removeInvoice
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +46,11 @@ describe('InvoiceController', () => {
       providers: [InvoiceService],
     }).compile();
 
-    controller = module.get<InvoiceController>(InvoiceController);
+    invoiceController = module.get<InvoiceController>(InvoiceController);
+    invoiceService = module.get<InvoiceService>(InvoiceService)
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(invoiceService).toBeDefined();
   });
 });
