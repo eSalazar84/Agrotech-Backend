c7756e3b57b1f9d80464f427a487119d1f71a1cd
Emiliano Salazar <salazaremiliano84@gmail.com>
2024-05-31 14:57:34 -0300

    testing sobre auth completo

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,147 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { AuthGuard } from './guard/auth.guard';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+import { IAccess_token } from './interface/auth.interface';
+import { Rol } from '../helpers/enums-type.enum';
+import { LoginDto } from './dto/login.dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let userService: UserService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(AuthGuard),
+          useValue: Repository
+        },
+        JwtService,
+        {
+          provide: UserService,
+          useValue: {
+            findUserByEmail: jest.fn(), // Mockea los métodos utilizados en AuthService
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
+    jwtService = module.get<JwtService>(JwtService)
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should login successfully', async () => {
+      const loginDto: LoginDto = {
+        email: 'test@gmail.com',
+        password: 'abc123',
+      };
+
+      const mockUser = {
+        idUser: 123,
+        email: 'test@gmail.com',
+        name: 'Test User',
+        lastname: 'lastname fantasy',
+        password: 'abc123',
+        rol: Rol.USER,
+        active: true,
+        createdAt: new Date(),
+        phone: '02281458596',
+        birthDate: new Date('1984-09-20')
+      };
+
+      const mockAccessToken: IAccess_token = {
+        access_token: 'fakeToken123',
+        email: 'test@gmail.com',
+        name: 'Test User',
+        rol: Rol.USER,
+        id: 123,
+      };
+
+      jest.spyOn(userService, 'findUserByEmail').mockResolvedValue(mockUser);
+      jest.spyOn(authService, 'comparePasswords').mockResolvedValue(true);
+      jest.spyOn(jwtService, 'signAsync').mockResolvedValue('fakeToken123');
+
+      const result = await authService.login(loginDto);
+      expect(result).toEqual(mockAccessToken);
+    });
+
+    it('should throw an exception when password is incorrect', async () => {
+      const loginDto: LoginDto = {
+        email: 'test@gmail.com',
+        password: 'abc123',
+      };
+
+      const mockUser = {
+        idUser: 123,
+        email: 'test@gmail.com',
+        name: 'Test User',
+        lastname: 'lastname fantasy',
+        password: 'hashedPassword',
+        rol: Rol.USER,
+        active: false,
+        createdAt: new Date(),
+        phone: '02281458596',
+        birthDate: new Date('1984-09-20')
+      };
+
+      jest.spyOn(userService, 'findUserByEmail').mockResolvedValue(mockUser);
+      jest.spyOn(authService, 'comparePasswords').mockResolvedValue(false);
+
+      await expect(authService.login(loginDto)).rejects.toThrow(HttpException);
+      await expect(authService.login(loginDto)).rejects.toMatchObject({
+        status: HttpStatus.UNAUTHORIZED,
+        response: {
+          status: HttpStatus.UNAUTHORIZED,
+          error: 'Contraseña o email incorrecto',
+        },
+      });
+    });
+
+    describe('Compare Password', () => {
+      it('should be password matched', async () => {
+        const loginDto: LoginDto = {
+          email: 'test@gmail.com',
+          password: 'abc123',
+        };
+
+
+        const mockUser = {
+          idUser: 123,
+          email: 'test@gmail.com',
+          name: 'Test User',
+          lastname: 'lastname fantasy',
+          password: 'abc123',
+          rol: Rol.USER,
+          active: false,
+          createdAt: new Date(),
+          phone: '02281458596',
+          birthDate: new Date('1984-09-20')
+        };
+
+        const hashedPassword = await bcrypt.hash(mockUser.password, 10)
+
+        jest.spyOn(authService, 'comparePasswords').mockResolvedValue(true)
+
+        const result = await authService.comparePasswords(loginDto.password, hashedPassword)
+
+        expect(result).toBe(true)
+      })
+
+      
+    })
   });
 });
--- src/invoice/invoice.service.spec.ts
+++ src/invoice/invoice.service.spec.ts
@@ -1,85 +1,100 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InvoiceService } from './invoice.service';
 import { Iinvoice } from './interface/invoice.entity';
+import { InvoicesDetailsService } from '../invoices_details/invoices_details.service';
+import { UserService } from '../user/user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Invoice } from './entities/invoice.entity';
+import { Repository, DataSource } from 'typeorm';
+import { InvoicesDetail } from '../invoices_details/entities/invoices_detail.entity';
+import { User } from '../user/entities/user.entity';
+import { IUser } from '../user/interface/user.interface';
+import { Rol } from '../helpers/enums-type.enum';
 
 describe('InvoiceService', () => {
   let invoiceService: InvoiceService;
-
-  const mockedArrayInvoice: Iinvoice[] = [
-    {
-      "invoiceDate": new Date("2024-05-21T23:20:50.000Z"),
-      "total_without_iva": 21216,
-      "total_with_iva": 25671,
-      "idInvoice": 1
-    },
-    {
-      "invoiceDate": new Date("2024-05-21T23:27:51.000Z"),
-      "total_without_iva": 21216,
-      "total_with_iva": 25671,
-      "idInvoice": 2
-    },
-    {
-      "invoiceDate": new Date("2024-05-21T23:28:09.000Z"),
-      "total_without_iva": 21216,
-      "total_with_iva": 25671,
-      "idInvoice": 3
-    }
-  ]
-
-  const mockInvoiceRepository = {
-    findAllInvoice: jest.fn(() => mockedArrayInvoice),
-    findOneInvoice: jest.fn((idInvoice: number) => mockedArrayInvoice.find(invoice => invoice.idInvoice === idInvoice)),
-    removeInvoice: jest.fn((id: number) => {
-      const invoiceIndex = mockedArrayInvoice.findIndex(invoice => invoice.idInvoice === id)
-      if (invoiceIndex !== -1) {
-        return mockedArrayInvoice.splice(invoiceIndex, 1)[0]
-      } else {
-        return null
-      }
-    })
-  }
+  let detailsService: InvoicesDetailsService;
+  let userService: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InvoiceService],
-    })
-      .overrideProvider(InvoiceService)
-      .useValue(mockInvoiceRepository)
-      .compile();
+      providers: [
+        InvoiceService,
+        {
+          provide: getRepositoryToken(Invoice),
+          useValue: Repository
+        },
+        InvoicesDetailsService,
+        {
+          provide: getRepositoryToken(InvoicesDetail),
+          useValue: Repository
+        },
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: Repository
+        },
+        {
+          provide: DataSource,
+          useValue: {
+            manager: {
+              transaction: jest.fn().mockImplementation((fn) => fn())
+            }
+          }
+
+        }
+      ],
+    }).compile()
 
     invoiceService = module.get<InvoiceService>(InvoiceService);
+    detailsService = module.get<InvoicesDetailsService>(InvoicesDetailsService)
+    userService = module.get<UserService>(UserService)
   });
 
   it('should be defined', () => {
     expect(invoiceService).toBeDefined();
   });
 
-  describe('Testing over read method', () => {
-    it('should be return all invoices', async () => {
-      const invoiceSpy = mockInvoiceRepository.findAllInvoice()
-      const invoiceReal = await invoiceService.findAllInvoice()
-      expect(invoiceSpy).toEqual(invoiceReal);
-    })
+  describe('Testing over create method', () => {
+    it('should be create a new invoice', async () => {
+      const userMock: IUser = {
+        idUser: 1,
+        name: 'Anabel',
+        lastname: 'Assann',
+        email: 'anabel@gmail.com',
+        phone: '2281513051',
+        birthDate: new Date('1984-05-13T13:54:00.000Z'),
+        createdAt: new Date('2024-03-18T13:54:00.000Z'),
+        active: true,
+        rol: Rol.USER,
+      }
 
-    it('should be return a one invoice', async () => {
-      const oneInvoiceSpy = mockInvoiceRepository.findOneInvoice(mockedArrayInvoice[1].idInvoice)
-      const oneInvoiceReal = await invoiceService.findOneInvoice(mockedArrayInvoice[1].idInvoice)
-      expect(oneInvoiceReal).toEqual(oneInvoiceSpy);
-    })
-  })
-
-  describe('Testing over delete method', () => {
-    it('should be delete a one invoice', async () => {
-      const invoiceToDelete = {
-        "invoiceDate": new Date("2024-05-21T23:28:09.000Z"),
+      const invoiceMock = {
+        "invoiceDate": new Date("2024-05-21T23:20:50.000Z"),
         "total_without_iva": 21216,
         "total_with_iva": 25671,
-        "idInvoice": 4
+        "idInvoice": 1
       }
-      const deleteInvoiceSpy = mockInvoiceRepository.removeInvoice(invoiceToDelete.idInvoice)
-      const deleteInvoiceReal = await invoiceService.removeInvoice(invoiceToDelete.idInvoice)
-      expect(deleteInvoiceReal).toEqual(deleteInvoiceSpy);
-      expect(!deleteInvoiceReal).toBeTruthy()
+
+      const detailsMock = {
+        "id": 1,
+        "amount_sold": 1
+      }
+
+
+      jest.spyOn(userService, 'findOneUser').mockResolvedValue(userMock)
+      jest.spyOn(invoiceService, 'createInvoice').mockRejectedValue(invoiceMock)
+
+      
+
+      
+      
+
+
+
+
     })
   })
+
+
 });
